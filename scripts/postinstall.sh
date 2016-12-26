40ee85eb61205b80c182d66ebd42fa51986575ca
Kostis Maninakis <maninak@plirono.com>
2016-12-26 12:03:32 +0200

    chore(grunt-ts): remove spammy console.log for each compiled file

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,3 +2,6 @@
 
 # Installs git hook that verifies commit messages comply with the angular convention
 ln -sfn ../../node_modules/angular-precommit/index.js .git/hooks/commit-msg
+
+# Removes spammy console.log for each single compiled file by grunt-ts package
+sed -i "s/^.*### Fast Compile.*$//" node_modules/grunt-ts/tasks/modules/compile.js
